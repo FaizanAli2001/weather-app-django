b4d6bdaf11e3345373e8490da78946da686b7f75
Faizan Ali <Faizanalyhh@gmail.com>
2026-08-12 03:52:08 +0500

    fix: handle invalid city errors

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -17,22 +17,43 @@ def home(request):
     }
 
     try:
-         data = request.get(url, params=PARAMS).json()
-        
-         description = data['weather'][0]['description']
-         icon = data['weather'][0]['icon']
-         temp = data['main']['temp']
-         day = datetime.date.today()
+        data = requests.get(url, params=PARAMS).json()
 
-         return render(request,'weatherapp/index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
-               
-    except:
-         exception_occured = True
-         messages.error(requests.request, 'City not found. Please enter a valid city name.')
-         day=datetime.date.today()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        day = datetime.date.today()
 
-         return render(request,'weatherapp/index.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'Lahore' , 'exception_occurred':True } )
+        return render(
+            request,
+            'weatherapp/index.html',
+            {
+                'description': description,
+                'icon': icon,
+                'temp': temp,
+                'day': day,
+                'city': city,
+                'exception_occurred': False
+            }
+        )
 
+    except Exception:
+        messages.error(
+            request,
+            'City not found. Please enter a valid city name.'
+        )
 
-   
+        day = datetime.date.today()
 
+        return render(
+            request,
+            'weatherapp/index.html',
+            {
+                'description': 'clear sky',
+                'icon': '01d',
+                'temp': 25,
+                'day': day,
+                'city': city,
+                'exception_occurred': True
+            }
+        )
