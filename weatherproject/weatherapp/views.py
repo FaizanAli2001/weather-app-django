0cb08c23760e71bbd50e0517fc8c1849e39b2df6
Faizan Ali <Faizanalyhh@gmail.com>
2026-08-12 03:07:28 +0500

    Frontend completed

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -2,23 +2,40 @@ from django.shortcuts import render
 import datetime
 import requests
 
-# Create your views here.
+
 def home(request):
 
-   if 'city' in request.POST:
+    if 'city' in request.POST:
         city = request.POST['city']
-   else:
-        city = 'Karachi'  # Default city if none provided
+    else:
+        city = 'Karachi'
 
-   url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={b187aa6eeb3b8ab732e256343aba5733}'
-   PARAMS = {'units': 'metric'}
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=bf9a041f15700d7dafd00c9990b23b29'
+    PARAMS = {
+        'units': 'metric'
+    }
 
-   data =request.get(url,PARAMS).json()
+    response = requests.get(url, params=PARAMS)
 
-   description = data['weather'][0]['description']
-   icon = data['weather'][0]['icon']
-   temp = data['main']['temp']
+    print("STATUS CODE:", response.status_code)
+    print("API RESPONSE:", response.json())
 
-   day = datetime.date.today()
+    data = response.json()
 
-   return render(request, 'weatherapp/index.html', {'description': description, 'icon': icon, 'temp': temp, 'day': day})
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+
+    day = datetime.date.today()
+
+    return render(
+        request,
+        'weatherapp/index.html',
+        {
+            'description': description,
+            'icon': icon,
+            'temp': temp,
+            'day': day,
+        }
+    )
+
